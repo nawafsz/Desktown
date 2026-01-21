661d4c2d8aa9b0e919c338684960a70f3a9ea818
OneDesk Developer <dev@onedesk.local>
2026-01-21 06:36:16 +0300

    Add client/index.htm and update build configuration

--- script/build.ts
+++ script/build.ts
@@ -1,6 +1,7 @@
 import { build as esbuild } from "esbuild";
 import { build as viteBuild } from "vite";
 import { rm, readFile } from "fs/promises";
+import path from "path";
 
 // server deps to bundle to reduce openat(2) syscalls
 // which helps cold start times
@@ -36,7 +37,9 @@ async function buildAll() {
   await rm("dist", { recursive: true, force: true });
 
   console.log("building client...");
-  await viteBuild();
+  await viteBuild({
+    configFile: path.resolve(process.cwd(), "vite.config.ts"),
+  });
 
   console.log("building server...");
   const pkg = JSON.parse(await readFile("package.json", "utf-8"));
